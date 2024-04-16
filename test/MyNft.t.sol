b25e5e5849a798cc3f0f38adfea5043542310c4a
Gryu <125647772+GRyu1@users.noreply.github.com>
2024-04-16 23:25:30 +0900

    721

---
+++ test/MyNft.t.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import "../src/MyNft.sol";
+import {stdStorage, StdStorage} from "forge-std/StdStorage.sol";
+
+contract MyNftTest is Test {
+    // library : simillar to contracts, but can not transfer eth and declare state variables.
+    using stdStorage for StdStorage;
+    MyNft private nft;
+
+    function setUp() public {
+        // Deploy NFT contract
+        nft = new MyNft("Cheong Keon Ryong", "Gryu", "https://fuchsia-administrative-elk-160.mypinata.cloud/ipfs/QmSrvyj5BjmpsmWmMueehYYiCtKC91bb9FuPHWc7zbtZpD/");
+    }
+
+    function test_RevertMintWithoutValue() public {
+        vm.expectRevert(MintPriceNotPaid.selector);
+        nft.mintTo(address(1));
+    }
+
+    function test_MintPricePaid() public {
+        nft.mintTo{value: 0.08 ether}(address(1)); // {} => input msg params
+        uint256 newOwnerData = stdstore
+            .target(address(nft))
+            .sig(nft.ownerOf.selector)
+            .with_key(address(1))
+            .find();
+
+        // load() : 
+        uint160 ownerOfTokenIdOne = uint160(
+            uint256(
+                (vm.load(address(nft), bytes32(abi.encode(newOwnerData))))
+            )
+        );
+        assertEq(address(ownerOfTokenIdOne), address(1));
+    }
+
+    function test_BalanceIncremented() public {
+        nft.mintTo{value: 0.08 ether}(address(1));
+        uint256 newOwnerData = stdstore
+            .target(address(nft))
+            .sig(nft.balanceOf.selector)
+            .with_key(address(1))
+            .find();
+
+        uint256 balanceFirstMint = uint256(
+            vm.load(address(nft), bytes32(newOwnerData))
+        );
+        assertEq(balanceFirstMint, 1);
+
+        nft.mintTo{value: 0.08 ether}(address(1));
+        uint256 balanceSecondMint = uint256(
+            vm.load(address(nft), bytes32(newOwnerData))
+        );
+        assertEq(balanceSecondMint, 2);   
+    }
+    
+    function test_SafeContractReceiver() public {
+        Receiver receiver = new Receiver();
+        nft.mintTo{value: 0.08 ether}(address(receiver));
+        uint256 slotBalance = stdstore
+            .target(address(nft))
+            .sig(nft.balanceOf.selector)
+            .with_key(address(receiver))
+            .find();
+
+        uint256 balance = uint256(vm.load(address(nft), bytes32(slotBalance)));
+        assertEq(balance, 1);
+    }
+
+    function test_RevertUnSafeContractReceiver() public {
+        // Adress set to 11, because first 10 addresses are restricted for precompiles (on Anvil wallet)
+        // function etch? : 
+        vm.etch(address(11), bytes("mock code"));
+        vm.expectRevert(bytes(""));
+        nft.mintTo{value: 0.08 ether}(address(11));
+    }
+
+    function test_WithdrawalWorksAsOwner() public {
+        // Mint an NFT, sending eth to the contract
+        Receiver receiver = new Receiver();
+        // 0x1337 : address?
+        address payable payee = payable(address(0x1337));
+        uint256 priorPayeeBalance = payee.balance;
+        nft.mintTo{value: nft.MINT_PRICE()}(address(receiver));
+        // Check that the balance of the contract is correct
+        assertEq(address(nft).balance, nft.MINT_PRICE());
+        uint256 nftBalance = address(nft).balance;
+        // Withdraw the balance and assert it was transferred
+        nft.withdrawPayments(payee);
+        assertEq(payee.balance, priorPayeeBalance + nftBalance);
+    }
+
+    function test_WithdrawalFailsAsNotOwner() public {
+        // Mint an NFT, sending eth to the contract
+        Receiver receiver = new Receiver();
+        nft.mintTo{value: nft.MINT_PRICE()}(address(receiver));
+        // Check that the balance of the contract is correct
+        assertEq(address(nft).balance, nft.MINT_PRICE());
+        // Confirm that a non-owner cannot withdraw
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.startPrank(address(0xd3ad));
+        nft.withdrawPayments(payable(address(0xd3ad)));
+        vm.stopPrank();
+    }
+}
+
+contract Receiver is ERC721TokenReceiver {
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 id,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
